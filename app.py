b2a9835fc6601f4ac0a3e32dec4421ddcb28246a
24f3002331 <24f3002331@ds.study.iitm.ac.in>
2025-10-04 21:32:55 +0530

    modifications to overcome circular import

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-## bcrypt
-
 from flask import Flask , render_template
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
+from models import db
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///hospital.db'
 app.config['SECRET_KEY'] = '250f7f1559a72d593f94f8814fab426f6aca7ec16e1e437c8d2ed6380ec0df8e'
 
-db = SQLAlchemy(app)
+db.init_app(app)
 bcrypt = Bcrypt(app)
 
 from models import *
-
 
 @app.route('/')
 def index():
